19d73da93dd0170fd799c70697af0af7f27e1728
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2025-10-03 11:11:52 -0500

    Add mock weather service and new Groq test for function call streaming

---
+++ src/main/java/dev/nathanlively/cheapest_llm_tool_calling/MockWeatherService.java
@@ -0,0 +1,63 @@
+package dev.nathanlively.cheapest_llm_tool_calling;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+
+public class MockWeatherService {
+
+    @Tool(description = "Get weather information for a specific location")
+    public WeatherResponse getWeather(
+            @ToolParam(description = "The city and state e.g. San Francisco, CA", required = true)
+            String location,
+            @ToolParam(description = "Temperature unit", required = true)
+            Unit unit) {
+
+        double temperature = 0;
+        if (location.contains("Paris")) {
+            temperature = 15;
+        } else if (location.contains("Tokyo")) {
+            temperature = 10;
+        } else if (location.contains("San Francisco")) {
+            temperature = 30;
+        }
+
+        return new WeatherResponse(temperature, 15, 20, 2, 53, 45, unit);
+    }
+
+    /**
+     * Temperature units.
+     */
+    public enum Unit {
+
+        /**
+         * Celsius.
+         */
+        C("metric"),
+        /**
+         * Fahrenheit.
+         */
+        F("imperial");
+
+        /**
+         * Human readable unit name.
+         */
+        public final String unitName;
+
+        Unit(String text) {
+            this.unitName = text;
+        }
+    }
+
+    /**
+     * Weather Function response.
+     */
+    public record WeatherResponse(
+            double temp,
+            double feels_like,
+            double temp_min,
+            double temp_max,
+            int pressure,
+            int humidity,
+            Unit unit) {
+    }
+}
--- src/test/java/dev/nathanlively/cheapest_llm_tool_calling/GroqTest.java
+++ src/test/java/dev/nathanlively/cheapest_llm_tool_calling/GroqTest.java
@@ -3,6 +3,8 @@ package dev.nathanlively.cheapest_llm_tool_calling;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClientResponse;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
@@ -27,9 +29,13 @@ public class GroqTest {
     private static final String GROQ_BASE_URL = "https://api.groq.com/openai";
     private static final String DEFAULT_GROQ_MODEL = "llama-3.1-8b-instant";
     private OpenAiChatModel chatModel;
+    protected ChatClient.Builder chatClientBuilder;
+    private ChatClient chatClient;
+    private MockWeatherService mockWeatherService;
 
     @BeforeEach
     void setUp() {
+        mockWeatherService = new MockWeatherService();
         chatModel = OpenAiChatModel.builder()
                 .openAiApi(OpenAiApi.builder()
                         .baseUrl(GROQ_BASE_URL)
@@ -38,12 +44,13 @@ public class GroqTest {
                 .defaultOptions(OpenAiChatOptions.builder()
                         .model(DEFAULT_GROQ_MODEL)
                         .build()).build();
+        chatClientBuilder = ChatClient.builder(chatModel);
+        chatClient = chatClientBuilder.build();
     }
 
     @Test
     void streamRoleTest() {
-        UserMessage userMessage = new UserMessage(
-                "Tell me about 3 famous pirates from the Golden Age of Piracy and what they did.");
+        UserMessage userMessage = new UserMessage("Tell me about 3 famous pirates from the Golden Age of Piracy and what they did.");
         SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate("""
                 You are a helpful AI assistant. Your name is {name}.
                 You are an AI assistant that helps people find information.
@@ -64,6 +71,28 @@ public class GroqTest {
                 .collect(Collectors.joining());
 
         assertThat(stitchedResponseContent).contains("Blackbeard");
+    }
+
+    @Test
+    void streamFunctionCallTest() {
+        final Flux<ChatClientResponse> chatClientResponseFlux = chatClient.prompt()
+                .system("You are a helpful AI assistant.")
+                .user("What's the weather like in San Francisco, Tokyo, and Paris? Return the temperature in Celsius.")
+                .tools(mockWeatherService)
+                .stream()
+                .chatClientResponse();
+
+        String content = Objects.requireNonNull(chatClientResponseFlux.collectList()
+                        .block())
+                .stream()
+                .map(ChatClientResponse::chatResponse).filter(Objects::nonNull)
+                .map(ChatResponse::getResults)
+                .flatMap(List::stream)
+                .map(Generation::getOutput)
+                .map(AssistantMessage::getText)
+                .collect(Collectors.joining());
+
+        assertThat(content).contains("30", "10", "15");
     }
 
 }
