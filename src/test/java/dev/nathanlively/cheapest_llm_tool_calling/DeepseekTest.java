72bd34c25bdecaf1b8d428f85a1832c8a2f48f1c
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2025-10-04 15:26:42 -0500

    Add `DeepseekTest` for smoke testing and introduce `spring-ai-starter-model-deepseek` dependency
    - Added `DeepseekTest` to validate functionality with `deepseek-chat` model.
    - Introduced `spring-ai-starter-model-deepseek` dependency in `pom.xml` to support DeepSeek integration.

--- src/test/java/dev/nathanlively/cheapest_llm_tool_calling/DeepseekTest.java
+++ src/test/java/dev/nathanlively/cheapest_llm_tool_calling/DeepseekTest.java
@@ -3,25 +3,25 @@ package dev.nathanlively.cheapest_llm_tool_calling;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.mistralai.MistralAiChatModel;
-import org.springframework.ai.mistralai.MistralAiChatOptions;
-import org.springframework.ai.mistralai.api.MistralAiApi;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
+import org.springframework.ai.deepseek.api.DeepSeekApi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@EnabledIfEnvironmentVariable(named = "MISTRALAI_API_KEY", matches = ".+")
-public class MistralTest {
+@EnabledIfEnvironmentVariable(named = "DEEPSEEK_API_KEY", matches = ".+")
+public class DeepseekTest {
 
     @Test
     void weatherServiceSmokeTest() {
         MockWeatherService weatherService = new MockWeatherService();
-        MistralAiApi mistralAiApi = new MistralAiApi(System.getenv("MISTRALAI_API_KEY"));
-        final MistralAiChatOptions mistralAiChatOptions = MistralAiChatOptions.builder()
-                .model(MistralAiApi.ChatModel.LARGE.getValue())
+        final DeepSeekChatOptions mistralAiChatOptions = DeepSeekChatOptions.builder()
+                .model(DeepSeekApi.ChatModel.DEEPSEEK_CHAT.getValue())
                 .temperature(0.1)
                 .build();
-        MistralAiChatModel chatModel = MistralAiChatModel.builder()
-                .mistralAiApi(mistralAiApi)
+        final DeepSeekApi deepseekApiKey = DeepSeekApi.builder().apiKey(System.getenv("DEEPSEEK_API_KEY")).build();
+        DeepSeekChatModel chatModel = DeepSeekChatModel.builder()
+                .deepSeekApi(deepseekApiKey)
                 .defaultOptions(mistralAiChatOptions)
                 .build();
         ChatClient chatClient = ChatClient.builder(chatModel).build();
