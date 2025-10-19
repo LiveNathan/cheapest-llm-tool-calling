0afb5f3dd7cffa06496156ee1d4f54621f2b1f40
Nathan Lively <nathan@squarewave.com.au>
2025-10-19 17:32:43 -0500

    Update Ollama test container models. Added and removed several models to test with.

--- src/test/java/dev/nathanlively/cheapest_llm_tool_calling/OllamaTestContainerProvider.java
+++ src/test/java/dev/nathanlively/cheapest_llm_tool_calling/OllamaTestContainerProvider.java
@@ -27,17 +27,18 @@ public class OllamaTestContainerProvider extends LlmProvider {
 //            "qwen3:0.6b",        // Smallest effective model for tool calling
 //            "qwen3:1.7b",
 //            "qwen3:4b",
+//            "qwen3:1.7b-q4_k_m"
 //            "qwen3:8b",
-//            "llama3.2:1b",
-//            "llama3.2:3b",
-//            "llama3.1:8b",
-            "orieg/gemma3-tools:1b",
+            "llama3.2:1b",
+            "llama3.2:3b",
+            "llama3.1:8b"
+//            "orieg/gemma3-tools:1b",
 //            "orieg/gemma3-tools:4b",
 //            "orieg/gemma3-tools:1b-it-qat",
 //            "orieg/gemma3-tools:4b-it-qat",
-            "okamototk/gemma3-tools:1b",
+//            "okamototk/gemma3-tools:1b",
 //            "okamototk/gemma3-tools:4b",
-            "phi4-mini:3.8b"
+//            "phi4-mini:3.8b"
     ));
 
     @Nullable
